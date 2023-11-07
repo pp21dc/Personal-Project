c257a1b0eacededc37d2f235c4b15a7ef797c772
Patrick Perry <93502969+pp21dc@users.noreply.github.com>
2023-11-07 11:14:03 -0500

    Started persistant scene work

---
+++ BTGD9856 Patrick Perry/Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get 
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+            }
+
+            if (!instance)
+            {
+                Debug.Log("No game manager Present!!!");
+            }
+            return instance; 
+        }
+    }
+
+
+    [SerializeField]
+    private string[] levelNames;
+
+    private bool isLoading = false;
+    private int currentLevel = 0;
+    private string currentLevelName;
+    void Start()
+    {
+        StartCoroutine("LoadLevel", levelNames[0]);
+    }
+
+    private IEnumerator LoadLevel(string levelName)
+    {
+        isLoading = true;
+
+        //uload current level scene
+
+
+        //load next level scene
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        currentLevelName = levelName;
+
+        //initialize player etc
+        isLoading = false;
+    }
+}
